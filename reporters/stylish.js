91cc3fc5d555d5769e76ab9ce3883e708c201bb7
Daniel Cousens <github@dcousens.com>
2015-07-16 13:10:22 +1000

    remove unnecessary functions/variables

--- reporters/stylish.js
+++ reporters/stylish.js
@@ -5,18 +5,16 @@ var gutil = require('gulp-util')
 var logSymbols = require('log-symbols')
 var path = require('path')
 var through2 = require('through2')
+var xtend = require('xtend')
 
 function Stylish (options) {
-  var opts = {
+  var opts = xtend({
     breakOnError: false,
     breakOnWarning: false
-  }
+  }, options)
 
   var totalErrorCount = 0
   var totalWarningCount = 0
-
-  // Extend and override default options with the ones user has set
-  for (var attr in options) { opts[attr] = options[attr] }
 
   // File specific reporter
   function reportFile (filepath, data) {
@@ -39,33 +37,15 @@ function Stylish (options) {
     return lines.join('\n') + '\n'
   }
 
-  // Reporter header
-  function reportHeader () {
-    console.log(colors.green('Standard linter results'))
-    console.log('======================================\n')
-  }
+  console.log(colors.green('Standard linter results'))
+  console.log('======================================\n')
 
-  // Reporter footer
-  function reportFooter () {
-    if (totalErrorCount === 0 && totalWarningCount === 0) {
-      console.log(logSymbols.success + ' ' + colors.green('All OK!'))
-    } else {
-      console.log('======================================')
-      console.log(logSymbols.error + colors.red(' Errors total: ' + totalErrorCount))
-      console.log(logSymbols.warning + colors.yellow(' Warnings total: ' + totalWarningCount) + '\n')
-    }
-  }
-
-  reportHeader()
-
-  return through2.obj(function (file, enc, cb) {
-    if (file.isNull()) {
-      return cb(null, file)
-    }
-
+  var stream = through2.obj(function (file, enc, callback) {
+    if (file.isNull()) return callback(null, file)
     if (file.isStream()) {
       this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streams are not supported!'))
-      return cb()
+
+      return callback()
     }
 
     // Report file specific stuff only when there are some errors/warnings
@@ -76,19 +56,29 @@ function Stylish (options) {
       console.log(reportFile(file.path, file.standard))
     }
 
-    cb()
+    callback()
   })
-    .on('end', function () {
-      reportFooter()
 
-      // If user wants gulp to break execution on reported errors or warnings
-      if (totalErrorCount && options.breakOnError) {
-        this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Linter errors occurred!'))
-      }
-      if (totalErrorCount && options.breakOnWarning) {
-        this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Linter warnings occurred!'))
-      }
-    })
+  stream.on('end', function () {
+    if (totalErrorCount === 0 && totalWarningCount === 0) {
+      console.log(logSymbols.success + ' ' + colors.green('All OK!'))
+
+    } else {
+      console.log('======================================')
+      console.log(logSymbols.error + colors.red(' Errors total: ' + totalErrorCount))
+      console.log(logSymbols.warning + colors.yellow(' Warnings total: ' + totalWarningCount) + '\n')
+    }
+
+    // If user wants gulp to break execution on reported errors or warnings
+    if (totalErrorCount && opts.breakOnError) {
+      this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Linter errors occurred!'))
+    }
+    if (totalErrorCount && opts.breakOnWarning) {
+      this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Linter warnings occurred!'))
+    }
+  })
+
+  return stream
 }
 
 module.exports = Stylish
